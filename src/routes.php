15dcf8045a31bf9dd8831fd261f32055d86c8efc
RosarioDeveloper <rosariomassango@gmail.com>
2020-10-13 09:57:38 +0100

    Actualizei

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
-<?php 
+<?php
 require_once MODULES_URL."auth/auth.routes.php";
 require_once MODULES_URL."categorias/categorias.routes.php";
 require_once MODULES_URL."accounts/accounts.routes.php";
+require_once MODULES_URL."store/store.routes.php";
 require_once MODULES_URL."store/store.routes.php";
